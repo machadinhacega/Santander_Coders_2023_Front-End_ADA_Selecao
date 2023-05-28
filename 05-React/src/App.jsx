6cebe129e3c6b505c1d24940a4f1a753b42fcc8c
iza machado <izav.machado@gmail.com>
2023-05-27 21:22:26 -0300

    React - aula 10

--- 05-React/src/App.jsx
+++ 05-React/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const tarefa = [
+  {id: '1', title: 'minha primeira tarefa'},
+  {id: '2', title: 'minha segunda tarefa'},
+  {id: '3', title: 'minha terceira tarefa'},
+  {id: '4', title: 'minha quarta tarefa'},
+]
+
+
+export default function App() {
+
+  const [tarefa, setTarefas] = useState([])
+
+  useEffect( () => {
+
+    async function buscarDados() {
+      const resultado = await fetch('https://jsonplaceholder.typicode.com/todos')
+      const resultadoFinal = await resultado.json()
+      return resultadoFinal
+    }
+
+    buscarDados().then(res => setTarefas(res))
+
+    // Esse 2º parâmetro "vazio" significa que:
+    // esse useEffect só vai ser executado quando meu componente App() for renderizado 1 unica vez
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Buscando Dados</h1>
+
+      <ol>
+      {tarefa.map( (tarefa) => {
+        return (
+          <div className="iten">
+            <li key={tarefa.id}>
+              {tarefa.completed ? <span><s>{tarefa.title}</s></span> : tarefa.title}
+
+              {/* {tarefa.title} */}
+              {/* {tarefa.completed ? <span><mark>Tarefa concluída</mark></span> : null} */}
+            </li>
+          </div>
+          )
+        })}
+      </ol>
+
+    </div>
   )
 }
-
-export default App
